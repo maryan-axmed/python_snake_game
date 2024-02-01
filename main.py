29666c45ad33bee3983419a07770008cbcc5c7a1
maryan <12maxmed@gmail.com>
2024-02-01 21:17:28 +0000

    snake can move, eat and grow

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import random
 
 # pygame setup
 pygame.init()
-square_width = 800
-pixel_width = 720
-screen = pygame.display.set_mode([square_width * 2])
+square_width = 750
+pixel_width = 50
+screen = pygame.display.set_mode([square_width] * 2)
 clock = pygame.time.Clock()
 running = True
 
@@ -33,6 +33,7 @@ snake = [snake_pixel.copy()]
 snake_direction = (0, 0)
 snake_length = 1
 
+# my target for the snake to eat
 target = pygame.rect.Rect([0, 0, pixel_width - 2, pixel_width - 2])
 target.center = generate_starting_position()
 
@@ -46,11 +47,42 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
-    # RENDER YOUR GAME HERE
+    if is_out_of_bounds():
+        snake_length = 1
+        target.center = generate_starting_position()
+        snake_pixel.center = generate_starting_position()
+        snake = [snake_pixel.copy()]
 
+    if snake_pixel.center == target.center:
+        target.center = generate_starting_position()
+        snake_length += 1
+        snake.append(snake_pixel.copy())
+
+    # RENDER YOUR GAME HERE
+    # these will move the snake.
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        snake_direction = (0, - pixel_width)
+    if keys[pygame.K_s]:
+        snake_direction = (0, pixel_width)
+    if keys[pygame.K_a]:
+        snake_direction = (- pixel_width, 0)
+    if keys[pygame.K_d]:
+        snake_direction = (pixel_width, 0)
+
+    for snake_part in snake:
+        pygame.draw.rect(screen, "green", snake_part)
+
+    pygame.draw.rect(screen, "red", target)
+
+    snake_pixel.move_ip(snake_direction)
+    snake.append(snake_pixel.copy())
+    snake = snake[-snake_length:]
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    # movement of the key clicks
+    clock.tick(5)  # limits FPS to 60
 
+# user clicks 'X' to close the window
 pygame.quit()
